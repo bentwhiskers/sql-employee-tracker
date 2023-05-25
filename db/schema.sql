b4c2363e7a9c14c8fcfabfd9ec6bcc8b8f3da62b
bentwhiskers <abbott1414@gmail.com>
2023-05-25 13:37:14 -0400

    created seeds for department and role in seeds.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -6,23 +6,22 @@ USE company_db;
 
 
 -- add table for department
-CREATE TABLE departments (
-    id INT PRIMARY KEY,
-    name VARCHAR(30),
-
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 -- add table for role
-CREATE TABLE roles (
-    id INT PRIMARY KEY,
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
 -- add table for employee
-CREATE TABLE employees (
-    id INT PRIMARY KEY,
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
-)
+);
