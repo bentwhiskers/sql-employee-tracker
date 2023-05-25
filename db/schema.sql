12ad060da5b2b5d7afa040f041b4fc0bfb7c5705
bentwhiskers <abbott1414@gmail.com>
2023-05-25 13:01:28 -0400

    created tables in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,28 @@
 -- create database
-
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 -- use database
-
--- see databse in use
-
+USE company_db;
 
 
 -- add table for department
+CREATE TABLE departments (
+    id INT PRIMARY KEY,
+    name VARCHAR(30),
 
+);
 -- add table for role
-
--- add table for employee 
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+-- add table for employee
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+)
